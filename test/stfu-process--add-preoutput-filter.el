5411bf5545ea6e5ece076a4a554f9c6075b5a15b
Dan Gillis <daniel.r.gillis@gmail.com>
2022-03-31 22:48:45 -0400

    Add test for custom preoutput filter placement

--- test/stfu-process--add-preoutput-filter.el
+++ test/stfu-process--add-preoutput-filter.el
@@ -6,7 +6,6 @@
     (stfu-process-mode)
     (should (equal comint-preoutput-filter-functions '(stfu-process-preoutput-filter identity)))))
 
-;; (ert-deftest stfu-process--append-preoutput-filter ())
 (ert-deftest stfu-process--append-preoutput-filter ()
   (with-temp-buffer
     (comint-mode)
@@ -16,4 +15,17 @@
     (should (equal comint-preoutput-filter-functions '(identity stfu-process-preoutput-filter)))))
 
 
-;; (ert-deftest stfu-process--custom-preoutput-filter ())
+
+(defun stfu-process--test-custom-placement (current-filter-list preoutput-filter)
+  (cons (car current-filter-list)
+        (cons preoutput-filter
+              (cdr current-filter-list))))
+
+
+(ert-deftest stfu-process--custom-placement-preoutput-filter ()
+  (with-temp-buffer
+    (comint-mode)
+    (setq comint-preoutput-filter-functions '(identity identity identity))
+    (setq stfu-process-add-filter-placement 'stfu-process--test-custom-placement)
+    (stfu-process-mode)
+    (should (equal comint-preoutput-filter-functions '(identity stfu-process-preoutput-filter identity identity)))))
