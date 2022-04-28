84e6017c17ff16d53ec5f36a34ce306e440a2fc0
Dan Gillis <daniel.r.gillis@gmail.com>
2022-04-28 10:27:46 -0400

    Add doc strings for filter placement tests

--- test/stfu-process--add-preoutput-filter.el
+++ test/stfu-process--add-preoutput-filter.el
@@ -1,4 +1,14 @@
+;; Test functions for adding preoutput filter to `comint-preoutput-filter-functions
+;; Note: Many or all of these tests are implementation dependent; all rely on
+;; setting `stfu-process-add-filter-placement` directly.
+
+(defun stfu-process--test-custom-placement (current-filter-list preoutput-filter)
+  (cons (car current-filter-list)
+        (cons preoutput-filter
+              (cdr current-filter-list))))
+
 (ert-deftest stfu-process--prepend-preoutput-filter ()
+  "Tests prepending of output filter to comint filter functions."
   (with-temp-buffer
     (comint-mode)
     (setq comint-preoutput-filter-functions '(identity))
@@ -7,6 +17,7 @@
     (should (equal comint-preoutput-filter-functions '(stfu-process-preoutput-filter identity)))))
 
 (ert-deftest stfu-process--append-preoutput-filter ()
+  "Tests appending of output filter to comint filter functions."
   (with-temp-buffer
     (comint-mode)
     (setq comint-preoutput-filter-functions '(identity))
@@ -14,15 +25,8 @@
     (stfu-process-mode)
     (should (equal comint-preoutput-filter-functions '(identity stfu-process-preoutput-filter)))))
 
-
-
-(defun stfu-process--test-custom-placement (current-filter-list preoutput-filter)
-  (cons (car current-filter-list)
-        (cons preoutput-filter
-              (cdr current-filter-list))))
-
-
 (ert-deftest stfu-process--custom-placement-preoutput-filter ()
+  "Tests use of custom function for placing output filter into comint filter functions."
   (with-temp-buffer
     (comint-mode)
     (setq comint-preoutput-filter-functions '(identity identity identity))
